67b6ec6bcd8d769205c0f2c6e32f8930f80bcc02
omnikoder <98806400+omnikoder@users.noreply.github.com>
2022-05-20 13:18:25 +0500

    feat: добавлены контроллер и представление для пользователей

--- src/main/java/root/Application.java
+++ src/main/java/root/Application.java
@@ -2,17 +2,11 @@ package root;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import root.repositories.UserRepositoryImpl;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		var context = SpringApplication.run(Application.class, args);
-		var userRepo = context.getBean(UserRepositoryImpl.class);
-
-		userRepo.getUsers().forEach(System.out::println);
-
-		context.close();
+		SpringApplication.run(Application.class, args);
 	}
 }
